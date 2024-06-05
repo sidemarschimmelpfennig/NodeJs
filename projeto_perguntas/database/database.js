ff5daf06119687ba031f9a93ab1fa0bcdacf4de5
Sidemar Junior <sidemarschimmelpfennig@outlook.com>
2024-06-05 08:01:25 -0300

    ajustes

--- projeto_perguntas/database/database.js
+++ projeto_perguntas/database/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const connection = new Sequelize('ajudaqui', 
+const connection = new Sequelize(process.env.DB_NAME, 
 process.env.DB_USERNAME, 
 process.env.DB_PASSWORD,{
     host : process.env.DB_HOST,
