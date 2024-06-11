17fbcb34a17084b8be0ed19f0411db18421efa81
Sidemar Junior <sidemarschimmelpfennig@outlook.com>
2024-06-10 21:57:27 -0300

    poo#3

--- poo/Composicao.js
+++ poo/Composicao.js
@@ -1,17 +1,47 @@
-class ManipularArquivo{
- constructor(nome){
-      this.arquivo = nome
- }
- LerArquivo(){
-    
- }
- EscrevaArquivo(){
 
- }
- CriarArquivo(){
-
- }
- DeletarArquivo(){
-
- }
+class Ler {
+     Ler(caminho){
+          console.log("Conteudo do arquivo")
+     }
 }
+
+class Criar {
+     Cre(caminho){
+          console.log(`Arquivo ${caminho} criado com sucesso.`)
+     }
+}
+
+class Escrever{  
+     Esc(caminho){
+          console.log(`Escrevendo artigo`)
+     }
+}
+
+class Delete {
+     Del(caminho){
+          console.log(`Arquivo ${caminho} deletado com sucesso.`)
+     }
+}
+
+
+
+
+
+
+class ManipularArquivo{
+     constructor(nome){
+          this.arquivo = nome
+          this.Leitor = new Ler()
+          this.Escritor = new Escrever()
+          this.Delete = new Delete()
+          this.Criador = new Criar()
+
+     }
+}
+
+let manipulador = new ManipularArquivo()
+
+manipulador.Criador.Cre('Dragão')
+manipulador.Delete.Del('Dragão')
+manipulador.Escritor.Esc()
+manipulador.Leitor.Ler()
